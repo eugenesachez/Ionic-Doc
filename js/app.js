fa9d8a9bed44d5cf7138c77b9b2d1800f19a48e9
Eugene <eugeniacsachez@gmail.com>
2016-09-07 10:11:13 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers','ionic-native-transitions'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -19,6 +19,14 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+  });
+})
+
+.config(function($ionicNativeTransitionsProvider){
+  $ionicNativeTransitionsProvider.setOptions({
+    "duration"          : 300,
+    "androiddelay"      : 100, // Longer delay better for older androids
+    // "fixedPixelsTop"    : 64, // looks OK on iOS
   });
 })
 
